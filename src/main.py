31f2289a14ddfe527f2d60ded39ec80de5cbf979
leonardogandrade <leonardo.g.andrade@gmail.com>
2023-08-16 15:20:41 -0300

    path

---
+++ src/main.py
@@ -0,0 +1,23 @@
+"""
+    Main Module docstring
+"""
+
+import os
+import uvicorn
+from fastapi import FastAPI
+from routes.chat import chat
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = {"http://localhost:3000", "http://localhost"}
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(chat)
